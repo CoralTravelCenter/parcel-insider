72b0b5bb1841dc5a828ded835b7c40d594d89501
shoore <shooore@gmail.com>
2023-04-03 16:34:24 +0300

    recent-favorite-jivo

---
+++ Resent-Favorite-Jivo/coral/backup/insider.js
@@ -0,0 +1,4 @@
+var dm_header = document.getElementsByTagName('body')[0];
+var dm_script = document.createElement('script');
+dm_script.src="//cdn.coral.ru/content/insider/russia/resent_favorite_jivo/resent_favorite_jivo-25.txt";
+dm_header.appendChild(dm_script);
