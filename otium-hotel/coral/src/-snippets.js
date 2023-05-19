072068116caf68f73cdf336646dcfe08800179af
shooore <shooore@gmail.com>
2023-05-19 18:05:37 +0300

    dev...

--- otium-hotel/coral/src/-snippets.js
+++ otium-hotel/coral/src/-snippets.js
@@ -1,7 +1,3 @@
 let visible_digits = $('.price-big').children().filter((idx, el) => !!el.clientWidth).toArray();
-visible_digits.sort((a, b) => {
-    const a_order = Number($(a).css('order'));
-    const b_order = Number($(b).css('order'));
-    return a_order < b_order ? -1 : a_order > b_order ? 1 : 0;
-});
+visible_digits.sort((a, b) => Number($(a).css('order')) - Number($(b).css('order')));
 let price = visible_digits.map(el => el.textContent).join('') * 1;
