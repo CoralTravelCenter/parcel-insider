4174e3dfe966b77c8b24a0d5542f5cf0887a2a66
shooore <shooore@gmail.com>
2023-06-14 16:06:09 +0300

    dev...

--- otium-hotel/coral/src/hotel-page.js
+++ otium-hotel/coral/src/hotel-page.js
@@ -15,7 +15,19 @@ if (hotel_data) {
 
     $markup.find('.bread-crumbs').empty().append($('.bcrumb'));
 
-    $markup.find('.visuals-gallery').empty().append($('[data-module="hotelgallery"]'));
+    const $original_hotelgallery_module = $('[data-module="hotelgallery"]');
+    const is_exclusive = !!$original_hotelgallery_module.find('.hotelgallery-wrap.exclusive').removeClass('exclusive').length;
+    const $visuals_gallery = $markup.find('.visuals-gallery');
+    $visuals_gallery.empty().append($original_hotelgallery_module);
+
+    let badges = [];
+    if (is_exclusive) badges.push('<div class="badge exclusive">EXCLUSIVE</div>');
+    if (hotel_data.SFC) badges.push('<a href="https://www.coral.ru/sunfamilyclub/" target="_blank" class="badge sfc"></a>');
+    if (badges.length) {
+        const $badges_stack = $('<div class="badges-stack"></div>').append(badges);
+        $visuals_gallery.append($badges_stack);
+    }
+
     let $hotel_name_location_rating = $('.header-left');
     $hotel_name_location_rating.find('.ratingWrapper').removeAttr('class').addClass('stars');
     $markup.find('.hotel-name-location').empty().append($hotel_name_location_rating);
