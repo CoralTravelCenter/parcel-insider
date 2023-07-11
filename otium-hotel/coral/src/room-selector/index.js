80010d0f9c6396a37d099bd8f43a65c2828b7e43
shoore <shooore@gmail.com>
2023-07-11 20:12:49 +0300

    dev...

--- otium-hotel/coral/src/room-selector/index.js
+++ otium-hotel/coral/src/room-selector/index.js
@@ -1,4 +1,5 @@
 import nights_selector_t from 'bundle-text:./nights-selector-template.html'
+import rooms_for_nights_t from 'bundle-text:./rooms-for-nights.html'
 import * as Mustache from "mustache";
 
 export class RoomSelector {
@@ -13,6 +14,7 @@ export class RoomSelector {
         this.$container = $(container);
         this.$roomsSelector = $(Mustache.render(nights_selector_t, { list: this.roomsRef }));
         this.$nightsSelector = this.$roomsSelector.filter('ul.selector');
+        this.$roomsHolder = this.$roomsSelector.filter('.rooms-holder');
         this.config = config;
         this.init();
     }
@@ -43,13 +45,27 @@ export class RoomSelector {
                 const dp = new DOMParser();
                 const doc = dp.parseFromString(response, 'text/html');
                 const rooms_nodes_list = doc.querySelectorAll('.room.row');
-                const rooms_data = [...rooms_nodes_list].map(room_node => {
+                const rooms_list = [...rooms_nodes_list].map(room_node => {
+                    const variant_nodes = room_node.querySelectorAll('.variant');
+                    const variants = [...variant_nodes].map(variant_node => {
+                        const meal_id = variant_node.getAttribute('data-mealid');
+                        const meal_name = variant_node.querySelector('.m-meal-name').textContent;
+                        return {
+                            meal: {
+                                id: meal_id,
+                                name: meal_name
+                            }
+                        }
+                    });
                     return {
                         name: room_node.querySelector('.roominfo h4')?.textContent,
-                        gallery_imagee: JSON.parse(room_node.querySelector('.roominfo .custom-gallery-wrapper').getAttribute('data-images'))
+                        gallery_imagee: JSON.parse(room_node.querySelector('.roominfo .custom-gallery-wrapper').getAttribute('data-images')),
+                        variants
                     };
                 });
-                resolve(rooms_data);
+                debugger;
+                this.$roomsHolder.append(Mustache.render(rooms_for_nights_t, { rooms_list }));
+                resolve(rooms_list);
             });
         });
         return rooms_ref.roomsData;
