84703dd59d49e390977add38e341abb1a52e429c
shooore <shooore@gmail.com>
2023-10-25 18:12:33 +0300

    @package-onlyhotel-switch -- Coral -- upd

--- package-onlyhotel-switch/coral/src/insider.js
+++ package-onlyhotel-switch/coral/src/insider.js
@@ -44,7 +44,88 @@ if (recentQuery.requestType !== 'onlyHotel') {
         $('.package-onlyhotel-switch').removeClass('yet-not-defined').find('a.onlyhotel').attr('href', response);
     });
 } else {
-    const recentQueryParams = recentQuery.onlyHotel;
+    let { Destination, BeginDate, EndDate, Guest } = recentQueryParams;
+    Destination.ModelType = 5;
+    Destination.RecordSourceType = 2;
+    const { name: departure_label, value: departure_id } = window.global.getActiveDeparture();
+    $.post('/v1/package/search', {
+        isCharter: true,
+        isRegular: false,
+        Departures: [{ Id: departure_id, Label: departure_label }],
+        Destination: [Destination],
+        BeginDate: moment(BeginDate).subtract({ d: 3 }).format('YYYY-MM-DD'),
+        EndDate: moment(BeginDate).add({ d: 3 }).format('YYYY-MM-DD'),
+        DateRange: 3,
+        SelectedDate: moment(BeginDate).format('YYYY-MM-DD'),
+        Acc: [moment.duration(moment(EndDate).diff(moment(BeginDate))).as('days')],
+        Guest,
+    }).done(response => {
+        $('.package-onlyhotel-switch').removeClass('yet-not-defined').find('a.package').attr('href', response);
+    });
 
 }
 
+let ONLYHOTEL_QUERY = {
+    "Destination": {
+        "Id": "Hotel8230",
+        "DataId": 8230,
+        "TopDataId": 31,
+        "ParentDataId": 840,
+        "FullTitle": "BAHI AJMAN PALACE HOTEL (BAHI AJMAN PALACE HOTEL)",
+        "Title": "BAHI AJMAN PALACE HOTEL",
+        "TitleRu": "BAHI AJMAN PALACE HOTEL",
+        "ParentTitle": "Ajman - Центр города, Ajman, United Arab Emirates",
+        "ParentTitleRu": "Аджман (Ajman) - Центр города, Аджман (Ajman), ОАЭ",
+        "Weight": 1,
+        "Score": 104.08000000001635,
+        "ModelType": "hotel",
+        "HasAirport": false,
+        "Priority": 4,
+        "RecordSourceType": "packageAndHotel",
+        "NearestAirports": [83, 139, 6650, 144, 87]
+    },
+    "BeginDate": "2023-12-20T00:00:00Z",
+    "EndDate": "2023-12-27T00:00:00Z",
+    "Guest": {
+        "Adults": 2,
+        "Children": []
+    },
+    "SearchedGuest": null,
+    "Hotels": null,
+    "HotelConcept": null,
+    "Page": 1,
+    "CurrencyId": 0
+};
+
+// isCharter: true
+// isRegular: false
+// Guest[Adults]: 2
+// SelectedDate: 2023-12-20
+// DateRange: 3
+// BeginDate: 2023-12-17
+// EndDate: 2023-12-23
+// Acc[]: 7
+
+// Departures[0][Id]: 2671
+// Departures[0][Label]: Москва
+
+// Destination[0][Id]: Hotel8230
+// Destination[0][DataId]: 8230
+// Destination[0][TopDataId]: 31
+// Destination[0][ParentDataId]: 840
+// Destination[0][FullTitle]: BAHI AJMAN PALACE HOTEL (BAHI AJMAN PALACE HOTEL)
+// Destination[0][Title]: BAHI AJMAN PALACE HOTEL
+// Destination[0][TitleRu]: BAHI AJMAN PALACE HOTEL
+// Destination[0][ParentTitle]: Ajman - Центр города, Ajman, United Arab Emirates
+// Destination[0][ParentTitleRu]: Аджман (Ajman) - Центр города, Аджман (Ajman), ОАЭ
+// Destination[0][Weight]: 1
+// Destination[0][Score]: 104.08000000001635
+// Destination[0][ModelType]: 5
+// Destination[0][HasAirport]: false
+// Destination[0][Priority]: 4
+// Destination[0][RecordSourceType]: 2
+// Destination[0][NearestAirports][]: 83
+// Destination[0][NearestAirports][]: 139
+// Destination[0][NearestAirports][]: 6650
+// Destination[0][NearestAirports][]: 144
+// Destination[0][NearestAirports][]: 87
