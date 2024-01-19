5813bb7a9dfe1c8643dc2aed826422f6c16e253e
shoore <shooore@gmail.com>
2024-01-19 11:44:46 +0300

    @popup-support-jan-2024 -- phase 2 -- done

--- popup-support-jan-2024/sunmar/src/insider.js
+++ popup-support-jan-2024/sunmar/src/insider.js
@@ -4,12 +4,12 @@ if (window[this_script_id]) throw `${ this_script_id } -> stop`;
 window[this_script_id] = true;
 // =====================================================================================================================
 
-import markup from 'bundle-text:./markup.html';
+import markup_phase1 from 'bundle-text:./markup-phase1.html';
+import markup_phase2 from 'bundle-text:./markup-phase2.html';
 import css from 'bundle-text:./insider.less';
 import { fetchNowMoment } from '/common/useful.js';
 
-const promo_href = 'https://www.sunmar.ru/info-actions/aktsiya-shedryj-yanvar/';
-
+let promo_href = 'https://www.sunmar.ru/info-actions/aktsiya-shedryj-yanvar/?erid=LjN8KPZoK';
 function phase(now_moment) {
     return now_moment.isBefore(moment('2024-01-19T16:00:00Z')) ? 1 : 2;
 }
@@ -21,27 +21,35 @@ function atOnlyhotel() {
     return ~self.location.pathname.indexOf('/onlyhotel/');
 }
 
+function atHome() {
+    return self.location.pathname === '/';
+}
+
 function rightCountry() {
     let cid;
+    const country_tab = ['turkey', 'uae', 'egypt', 'thailand'];
     if (atPackage()) {
         cid = Number($('input.packageSearch__destinationInput').attr('countryid'));
-        return ~[1, 12, 31, 33].indexOf(cid);
+        return country_tab[[1, 31, 12, 33].indexOf(cid)];
     } else if (atOnlyhotel() && window.insider_object?.listing?.items?.length) {
         const country_part = window.insider_object.listing.items[0].url.split('/')[0];
-        return ~['turkey', 'united-arab-emirates', 'egypt', 'thailand'].indexOf(country_part);
+        return country_tab[['turkey', 'united-arab-emirates', 'egypt', 'thailand'].indexOf(country_part)];
+    } else if (atHome()) {
+        cid = Number($('input.packageSearch__destinationInput').attr('countryid'));
+        return country_tab[[1, 31, 12, 33].indexOf(cid)];
     }
     return false;
 }
 
-function departOnJanuary() {
-    let depart_moment;
-    if (atPackage()) {
-        depart_moment = moment($('#departureDateRangePickerInput').val().split(/\D/).reverse().join('-'));
-    } else if (atOnlyhotel()) {
-        depart_moment = moment($('input.datepicker-input').val().split(/\s*-\s*/)[0].split(/\D/).reverse().join('-'));
-    }
-    return depart_moment.isSame('2024-01-01', 'month');
-}
+// function departOnJanuary() {
+//     let depart_moment;
+//     if (atPackage()) {
+//         depart_moment = moment($('#departureDateRangePickerInput').val().split(/\D/).reverse().join('-'));
+//     } else if (atOnlyhotel()) {
+//         depart_moment = moment($('input.datepicker-input').val().split(/\s*-\s*/)[0].split(/\D/).reverse().join('-'));
+//     }
+//     return depart_moment.isSame('2024-01-01', 'month');
+// }
 
 function shownInSession() {
     return sessionStorage.getItem(this_script_id);
@@ -62,14 +70,26 @@ function incrementShownToday(now_moment) {
 
 (async function () {
     const now = moment();
-    if (phase(now) === 1 && rightCountry() && !shownInSession() && showToday(now) < 3) {
-    // if (phase(now) === 1 && rightCountry()) {
+    if (phase(now) === 1 && (atPackage() || atOnlyhotel()) && rightCountry() && !shownInSession() && showToday(now) < 3) {
         $('head').append(`<style>${ css }</style>`);
-        $('body').append(markup);
+        $('body').append(markup_phase1);
         const $modal = $('#popup-support-jan-2024-modal');
         $('[data-action="dismiss"]').on('click', () => $modal.modal('hide'));
         $modal.modal();
-        sessionStorage.setItem(this_script_id, 1);
+        sessionStorage.setItem(this_script_id, 'v2');
+        incrementShownToday(now);
+    } else if (phase(now) === 2 && atHome() && !shownInSession() && showToday(now) < 3) {
+        $('head').append(`<style>${ css }</style>`);
+        $('body').append(markup_phase2);
+        const $modal = $('#popup-support-jan-2024-modal');
+        $('[data-action="dismiss"]').on('click', () => $modal.modal('hide'));
+        const searched_country = rightCountry();
+        if (searched_country) {
+            promo_href += '&country=' + searched_country;
+        }
+        $modal.find('a.action').attr('href', promo_href);
+        $modal.modal();
+        sessionStorage.setItem(this_script_id, 'v2');
         incrementShownToday(now);
     }
 })();
