6ed8029c01a6eadf8327e930df032c78b7b0413f
shooore <shooore@gmail.com>
2025-04-16 14:36:13 +0300

    ... warn-if-no-transfer + sunmar (for TagManager)

--- @mindbox/warn-if-no-transfer/sunmar/src/mindbox.js
+++ @mindbox/warn-if-no-transfer/sunmar/src/mindbox.js
@@ -1,15 +1,9 @@
-// =====================================================================================================================
-const this_script_id = 'warn-if-no-transfer';
-if (window[this_script_id]) throw `${ this_script_id } -> stop`;
-window[this_script_id] = true;
-// =====================================================================================================================
-
 import domWarden from './dom-warden.js';
 import msg_template from 'bundle-text:./templates/pay-attention-markup.html'
 import styles from 'bundle-text:./templates/styles.less';
 
 (function () {
-
+    console.warn("@minbox/warn-if-no-transfer/coral");
     domWarden(noTransferFlagEl, (node) => {
         if (node instanceof HTMLElement) {
             return noTransferFlagEl();
@@ -17,10 +11,13 @@ import styles from 'bundle-text:./templates/styles.less';
         return false;
     }, () => {
         const no_transfer_flag_el = noTransferFlagEl();
-        if (!no_transfer_flag_el.getBoundingClientRect().height) return;
+        if (!no_transfer_flag_el) return;
+        if (no_transfer_flag_el.classList.contains('mb-checked')) return;
 
         // console.log('*** PAYLOAD SHOULD EXEC ***');
-        noTransferFlagEl().style.display = 'none';
+        // noTransferFlagEl().style.display = 'none';
+        no_transfer_flag_el?.classList.add('mb-checked');
+
         const service_block_el = transferAddServiceBlock();
         service_block_el.insertAdjacentHTML('beforeend', msg_template);
         service_block_el.querySelector('.content-body')?.remove();
@@ -30,11 +27,15 @@ import styles from 'bundle-text:./templates/styles.less';
     });
 
     function noTransferFlagEl() {
-        const no_transfer_flag_el = [...document.querySelectorAll('.product-summary-row')].find((el) => {
-            return el.textContent.indexOf('Доплата за трансфер') === 0;
-        });
-        const displayed_price = parseFloat(no_transfer_flag_el?.querySelector('.price').textContent);
-        return !!no_transfer_flag_el && displayed_price === 0.0 ? no_transfer_flag_el : null;
+        const explicit_no_transfer_row = document.querySelector('.product-summary-row:has(.price.not-include)');
+        if (!explicit_no_transfer_row) {
+            // if not -- check for complete absence "transfer" row in case of "hotel only"
+            debugger;
+            const summary_rows = document.querySelectorAll('.product-summary-row');
+            const there_is_no_transfer_row = [...summary_rows].every(row => row.textContent.trim().indexOf('Трансфер') !== 0);
+            return there_is_no_transfer_row ? summary_rows[0] : null;
+        }
+        return explicit_no_transfer_row;
     }
 
     function transferAddServiceBlock() {
