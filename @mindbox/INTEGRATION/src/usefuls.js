9096eb5dd7b13bd978afb853a667274a245eab85
shooore <shooore@gmail.com>
2025-05-21 11:40:46 +0300

    + INTEGRATION -- Website.ViewCategory

---
+++ @mindbox/INTEGRATION/src/usefuls.js
@@ -0,0 +1,80 @@
+
+export async function hostReactAppReady(selector = '#__next > div', timeout = 500) {
+    return new Promise(resolve => {
+        const waiter = () => {
+            const host_el = document.querySelector(selector);
+            if (host_el?.getBoundingClientRect().height) {
+                resolve();
+            } else {
+                setTimeout(waiter, timeout);
+            }
+        };
+        waiter();
+    });
+}
+
+export async function waitForSelector(selector, interaval = 500) {
+    return new Promise(resolve => {
+        const waiter = () => {
+            const el = document.querySelector(selector);
+            if (el) {
+                resolve(el);
+            } else {
+                setTimeout(waiter, interaval);
+            }
+        };
+        waiter();
+    });
+}
+
+export async function asap(cb) {
+    if (['complete', 'interactive'].includes(document.readyState)) {
+        cb && cb();
+        return Promise.resolve();
+    }
+    return new Promise(resolve => {
+        document.addEventListener('DOMContentLoaded', () => {
+            cb && cb();
+            resolve();
+        });
+    });
+}
+
+export function myCookie(name, source = document.cookie) {
+    const cookies_list = source.split(/;\s+/);
+    const cookies = {};
+    for (const cookie_str of cookies_list) {
+        const [name, value] = cookie_str.split('=');
+        cookies[name] = decodeURIComponent(value);
+    }
+    return name ? cookies[name] : cookies;
+}
+
+export function queryParam(p, source) {
+    source ||= location.href;
+    let [url, query] = source.split('?');
+    query ||= '';
+    const params_kv = query.split('&');
+    const params = {};
+    for (const kv of params_kv) {
+        let [k, v] = kv.split('=');
+        try {
+            v = decodeURIComponent(v);
+            v = JSON.parse(v);
+        } catch (ex) {}
+        params[k] = v;
+    }
+    if (p) {
+        return params[p];
+    } else {
+        return params;
+    }
+}
+
+export function params2query(p) {
+    const kv = [];
+    for (let [k, v] of Object.entries(p)) {
+        kv.push(`${ k }=${ encodeURIComponent(typeof v === 'object' ? JSON.stringify(v) : v) }`);
+    }
+    return kv.join('&');
+}
---
+++ @mindbox/INTEGRATION/src/view-category.js
@@ -0,0 +1,48 @@
+import { myCookie, params2query, queryParam } from "./usefuls";
+
+const api_host = {
+    'www.coral.ru': 'b2capi.coral.ru',
+    'b2cpilotui.coral.ru': 'b2cpilotapi.coral.ru'
+}[location.hostname];
+
+(async function () {
+    const { qp, p } = queryParam();
+    const isPackageTour = p == 1;
+    const decrypt_endpoint = isPackageTour ? 'PackageTourHotelProduct' : 'OnlyHotelProduct';
+    location.hostname.indexOf('pilot')
+    const query = await fetch(`//${ api_host }/${ decrypt_endpoint }/PriceSearchDecrypt`, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ queryParam: encodeURIComponent(qp) })
+    }).then(response => response.json());
+
+    let viewed_countries = [];
+    try {
+        const { result: { searchCriterias: { arrivalLocations } } } = query;
+        viewed_countries = arrivalLocations.map(loc => {
+            const country_id_src = loc.type === 0 ? loc.id : loc.parent.countryId;
+            return country_id_src.split('-').at(0);
+        });
+        viewed_countries = [...new Set(viewed_countries)];
+    } catch (ex) {
+        console.warn('*** Failed to parse/process query: %o', query);
+    }
+
+    try {
+        viewed_countries.forEach(country_id => {
+            const qparams = params2query({
+                endpointId: 'coral-stage.Website',
+                operation: "Website.ViewCategory",
+                deviceUUID: myCookie('mindboxDeviceUUID')
+            });
+            fetch(`//api.s.mindbox.ru/v3/operations/async?${ qparams }`, {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json; charset=utf-8' },
+                body: JSON.stringify({ viewProductCategory: { productCategory: { ids: { hotels: country_id } } } })
+            });
+        });
+    } catch (ex) {
+        console.warn('*** Failed to invoke operation Website.ViewCategory: %o', ex);
+    }
+
+})();
